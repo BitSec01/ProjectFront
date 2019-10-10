1625c024c062e6a29e21783900db6d88476ea3f3
Roel <35380022+RoelLeijser@users.noreply.github.com>
2019-10-10 14:15:00 +0200

    bedrijvenpagina

---
+++ src/components/CompanyTable.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-body">
+          <h4 class="header-title">Bedrijven Tabel</h4>
+          <p class="text-muted font-13 mb-4">
+            hier kun je alle bedrijven zien en managen. je kunt sorteren door op de column te klikken en zoeken met de
+            zoekbalk!
+          </p>
+
+          <DataTableCompany @companySelected="handleEmit"/>
+
+        </div> <!-- end card body-->
+      </div> <!-- end card -->
+    </div><!-- end col-->
+  </div>
+  <!-- end row-->
+</template>
+
+<script>
+  import Axios from "axios";
+  import DataTableCompany from "@/components/DataTableCompany";
+
+  export default {
+    name: 'CompanyTable',
+    data() {
+      return {
+        companySelected: ""
+      }
+    },
+    components: {
+      DataTableCompany
+    },
+    methods: {
+      handleEmit(event) {
+        console.log('data after child handle: ', event) // get the data after child dealing
+        this.$emit('companySelected', event);
+      }
+    }
+  }
+</script>
