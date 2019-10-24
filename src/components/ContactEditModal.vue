bb02adb63e460709af7a39ba9ec45678c8e8c2f8
TheLuckyCubes <Lucvogelsangs@gmail.com>
2019-10-24 13:45:18 +0200

    Edit Contacts

---
+++ src/components/ContactEditModal.vue
@@ -0,0 +1,143 @@
+<template>
+  <div id="contactModal" class="modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">Contact: {{ contact.name }} {{ contact.middlename }} {{ contact.lastname }}</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true" v-on:click="closeModal()">×</button>
+        </div>
+        <div class="modal-body p-4">
+          <div class="row">
+
+            <div class="col-md-2">
+              <div class="form-group">
+                <label for="class" class="control-label">Company id</label>
+                <input type="text" class="form-control" id="class" v-model="contact.company_id">
+              </div>
+            </div>
+            <div class="col-md-5">
+              <div class="form-group">
+                <label for="field-1" class="control-label">Name</label>
+                <input type="text" class="form-control" id="field-1" v-model="contact.name">
+              </div>
+            </div>
+            <div class="col-md-5">
+              <div class="form-group">
+                <label for="field-2" class="control-label">Lastname</label>
+                <input type="text" class="form-control" id="field-2" v-model="contact.lastname">
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="phone" class="control-label">Telefoon</label>
+                <input type="text" class="form-control" id="phone" v-model="contact.phone">
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="email" class="control-label">Email</label>
+                <input type="text" class="form-control" id="email" v-model="contact.email">
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="phone" class="control-label">Laatst gecontacteerd</label>
+                <input type="date" class="form-control" id="last_contacted" v-model="contact.last_contacted">
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="email" class="control-label">Gecontacteerd door</label>
+                <input type="text" class="form-control" id="contacted_by" v-model="contact.contacted_by">
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-12">
+              <div class="form-group no-margin">
+                <label for="field-7" class="control-label">Is begeleider?</label><br/>
+                <span v-if="contact.is_supervisor" class="badge badge-success">Ja</span>
+                <span v-else class="badge badge-danger">Nee</span>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary waves-effect" data-dismiss="modal" v-on:click="closeModal()">Close</button>
+          <button type="button" class="btn btn-info waves-effect waves-light" v-on:click="SubmitModal()">Save changes</button>
+          <button type="button" class="btn btn-danger waves-effect waves-light" v-on:click="Delete()">Delete this contact</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Axios from 'axios';
+export default {
+  name: 'Modal',
+  props: ['contact'],
+  methods: {
+    closeModal() {
+      event.preventDefault();
+      $('#contactModal').modal('hide');
+    },
+    SubmitModal() {
+      console.log(this.contact.id)
+      Axios.patch('http://localhost:80/contact/' + this.contact.id, {
+        name: this.contact.name,
+        middlename: this.contact.middlename,
+        lastname: this.contact.lastname,
+        email: this.contact.email,
+        phone: this.contact.phone,
+        company_id: this.contact.company_id,
+        last_contacted: this.contact.last_contacted,
+        contacted_by: this.contact.contacted_by,
+        is_supervisor: this.contact.is_supervisor
+      }).then((response) => {
+        console.log(this.contact);
+        console.info('Contact succesfully updated');
+        console.log(response.data);
+      }).catch((err) => {
+        console.warn(err);
+        alert('Error has occured check log');
+      }).finally(() => {
+        console.log('Hello world');
+        //$('#contactModal').modal('hide');
+        //setTimeout("location.reload(true);", 1);
+      });
+    },
+    Delete() {
+      Axios.delete('http://localhost:80/contact/' + this.contact.id).then((response) => {
+        console.log(this.contact);
+        console.info('Contact succesfully deleted');
+        console.log(response.data);
+      }).catch((err) => {
+        console.warn(err);
+        alert('Error has occured check log');
+      }).finally(() => {
+        console.log('Hello world');
+        //$('#contactModal').modal('hide');
+        //setTimeout("location.reload(true);", 1);
+      });
+    }
+  }
+}
+</script>
+
+<style scoped>
+.modal-dialog {
+  max-width: 900px;
+}
+.badge {
+  font-size: 14px;
+}
+</style>
--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,36 +1,29 @@
 <template>
   <div>
     <TopNav />
-  <div class="wrapper">
-    <div class="container-fluid">
-      
-      <div class="row">
-        <div class="col-12">
-          <div class="page-title-box">
-            <div class="page-title-right">
-              <ol class="breadcrumb m-0">
-                <li class="breadcrumb-item"><a href="javascript: void(0);">UBold</a></li>
-                <li class="breadcrumb-item"><a href="javascript: void(0);">Tables</a></li>
-                <li class="breadcrumb-item active">Datatables</li>
-              </ol>
+    <div class="wrapper">
+      <div class="container-fluid">
+
+        <div class="row">
+          <div class="col-12">
+            <div class="page-title-box">
+              <h4 class="page-title">Datatables</h4>
             </div>
-            <h4 class="page-title">Datatables</h4>
           </div>
         </div>
+
+        <ContactsTable @contactSelected="handleEmit"/>
+
       </div>
-
-      <ContactsTable @contactSelected="handleEmit"/>
-
     </div>
-  </div>
-    <Modal v-bind:contact="contact"/>
+    <ContactEditModal v-bind:contact="contact"/>
   </div>
 </template>
 
 <script>
 import TopNav from '@/components/TopNav';
 import ContactsTable from '@/components/ContactsTable';
-import Modal from '@/components/Modal';
+import ContactEditModal from '@/components/ContactEditModal';
 
 export default {
   name: 'Contacts',
@@ -42,7 +35,7 @@ export default {
   components: {
     TopNav,
     ContactsTable,
-    Modal
+    ContactEditModal
   },
   methods: {
     handleEmit(event) {
